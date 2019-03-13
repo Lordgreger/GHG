6938f58fc8188596e6061489651f3174e4b2e313
Greger <blacknedpsi@gmail.com>
2019-03-13 11:35:01 +0100

    kjjhkjkh

--- DatProject/Assets/TerrainCreator.cs
+++ DatProject/Assets/TerrainCreator.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public static class AreaConstants {
+    public const int tileResolution = 32;
+    public const int depth = 100;
+    public const int width = 128;
+    public const int height = 128;
+}
+
 public class TerrainCreator : MonoBehaviour {
     public TerrainLayer grass;
-
-    const int depth = 100;
-    const int width = 64;
-    const int height = 64;
-    const int tileResolution = 8;
-
 
 	void Start () {
         createGrasslands();
@@ -17,22 +18,26 @@ public class TerrainCreator : MonoBehaviour {
 
     void createGrasslands() {
         TerrainData td = new TerrainData();
-        td.size = new Vector3(width, depth, height);
-        td.heightmapResolution = width + 1;
+        td.size = new Vector3(AreaConstants.width, AreaConstants.depth, AreaConstants.height);
+        td.heightmapResolution = AreaConstants.width + 1;
 
         td.terrainLayers = new TerrainLayer[] { grass };
-        createHeights(td);
+
+        AreaData ad = new AreaData(AreaData.Type.hills, AreaConstants.tileResolution);
+
+        createHeights(td, ad);
 
         Terrain.CreateTerrainGameObject(td);
     }
 
-    void createHeights(TerrainData td) {
-        float[,] hm = new float[width, height];
-        int tileSize = width / tileResolution;
+    void createHeights(TerrainData td, AreaData ad) {
+        float[,] hm = new float[AreaConstants.width, AreaConstants.height];
+        int tileSize = AreaConstants.width / AreaConstants.tileResolution;
+        float scale = (1f / (float)AreaConstants.depth);
 
-        for (int x = 0; x < tileResolution; x++) {
-            for (int y = 0; y < tileResolution; y++) {
-                setTile(hm, x * tileSize, y * tileSize, tileSize, (1f / (float)depth) * Random.Range(0, 2));
+        for (int x = 0; x < AreaConstants.tileResolution; x++) {
+            for (int y = 0; y < AreaConstants.tileResolution; y++) {
+                setTile(hm, x * tileSize, y * tileSize, tileSize, scale * ad.heights[x, y]);
             }
         }
 
@@ -46,5 +51,63 @@ public class TerrainCreator : MonoBehaviour {
             }
         }
     }
+}
+
+public class AreaData {
+
+    public int[,] heights;
+
+    public enum Type {
+        hills
+    }
+
+    public AreaData(Type t, int resolution) {
+        heights = new int[resolution, resolution];
+
+        switch (t) {
+
+            case Type.hills:
+                createHills();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    void setZero() {
+        for (int i = 0; i < heights.GetLength(0); i++) {
+            for (int j = 0; j < heights.GetLength(1); j++) {
+                heights[i, j] = 0;
+            }
+        }
+    }
+
+    void createHills() {
+        const int sizeMin = 4;
+        const int sizeMax = 9;
+        const int countMin = 1;
+        const int countMax = 4;
+
+        setZero();
+
+        createHill(2, 2, 4);
+
+
+
+    }
+
+    void createHill(int x, int y, int size) {
+        for (int i = 0; i < heights.GetLength(0); i++) {
+            for (int j = 0; j < heights.GetLength(1); j++) {
+                float pointSize = (float)size - Mathf.Sqrt(Mathf.Pow((float)i - (float)x, 2) + Mathf.Pow((float)j - (float)y, 2));
+                if (heights[i, j] < pointSize) {
+                    heights[i, j] = Mathf.RoundToInt(pointSize);
+                }
+            }
+        }
+    }
+
+    
 
 }
